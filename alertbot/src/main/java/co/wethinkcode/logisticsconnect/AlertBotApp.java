db2f22a0f0f0bece1be3ccc8cbb2c165ce49ec4d
nktshjhb025 <nktshjhb025@student.wethinkcode.co.za>
2026-09-24 19:59:36 +0200

    feat: add AlertBot skeleton

--- alertbot/src/main/java/co/wethinkcode/logisticsconnect/AlertBotApp.java
+++ alertbot/src/main/java/co/wethinkcode/logisticsconnect/AlertBotApp.java
@@ -1,16 +1,49 @@
 package co.wethinkcode.logisticsconnect;
 
 import io.javalin.Javalin;
+import co.wethinkcode.logisticsconnect.mq.MqConfig;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
 
 public class AlertBotApp {
 
-    public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7054);
+    /**Stage 5 >= Alert will be posted
+     * Stage 5 < Alert will not be posted
+     * **/
 
+    private static final int ALERT_THRESHOLD = 5;
+
+
+    public static void main(String[] args) throws Exception {
+        startSubscriber();
+
+        Javalin app = Javalin.create().start(7054);
         app.get("/health", ctx -> ctx.result("OK"));
+    }
 
         // TODO (Posts proactive delay notifications to public transit social media pages (simulated).)
         // Mechanism: Outbound webhook, simulated social post
+        private static void startSubscriber() throws JMSException {
+            ConnectionFactory factory = new ActiveMQConnectionFactory(MqConfig.BROKER_URL);
+            Connection connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create and Subscribe to the Topic
+            Topic topic = session.createTopic(MqConfig.TOPIC);
+            MessageConsumer consumer = session.createConsumer(topic);
+
+            consumer.setMessageListener(msg -> {
+
+
+
+
+            });
+
+
+
     }
 }
 
